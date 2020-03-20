1e706f582cf1c5cb60c5da599bf30b36f4fbaafb
webbhost610 <62347821+webbhost610@users.noreply.github.com>
2020-03-20 13:13:36 -0500

    Create custom.js

---
+++ Sundarakanda-Sargas/scripts/custom.js
@@ -0,0 +1,27 @@
+"use strict";
+var test = {};
+web.includeHTML = function(cb) {
+	alert("ENTERED");
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+			alert(this.status);
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          elmnt.removeAttribute("include-html");
+          test.includeHTML(cb);
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+  if (cb) cb();
+};
